83a822d103a347d60801d5cb4ff137644a1cf581
Karim Kidiss <ozerioss@gmail.com>
2017-01-03 15:08:34 +0100

    Update Snippets.js

--- Snippets.js
+++ Snippets.js
@@ -1,1 +1,35 @@
-document.write("Random # (1-10) = ", Math.floor((Math.random() * 100) + 1, "<br/>"));
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <script src="jswork.js"></script>
+ 
+    <style type="text/css">
+      body {font-size: 1.6em;}
+      .hidden {display:none;}
+      .show {display:inline !important;}
+      button {
+        border: 2px solid black; background: #E5E4E2;
+        font-size: .5em; font-weight: bold; color: black;
+        padding: .8em 2em;
+        margin-top: .4em;
+      }
+	</style>
+	
+	</head>
+	
+	<body>
+	
+  
+  <p id="sayHello"></p>
+  <script>
+    
+    // Allows us to output random number between 1 and 100
+    document.write("Random # (1-10) = ", Math.floor((Math.random() * 100) + 1, "<br/>"));
+
+  </script>
+	
+	</body>
+	
+	
+</html>
